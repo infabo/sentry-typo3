fb57195f9c8486dd37a28b43c6ccb945db64fee0
Ingo Fabbri <inf@mmcagentur.at>
2022-12-20 18:59:36 +0100

    [TASK] mirgate deprecated usage of TYPO3_version

--- src/Integration/Typo3Context.php
+++ src/Integration/Typo3Context.php
@@ -6,6 +6,7 @@ namespace Helhum\SentryTypo3\Integration;
 
 use Sentry\Event;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 class Typo3Context implements ContextInterface
 {
@@ -17,8 +18,8 @@ class Typo3Context implements ContextInterface
     public function addToEvent(Event $event): void
     {
         $event->setTags(array_merge_recursive($event->getTags(), [
-            'typo3_version' => TYPO3_version,
             'typo3_mode' => TYPO3_MODE,
+            'typo3_version' => (new Typo3Version())->getVersion(),
             'application_context' => (string)Environment::getContext(),
             'application_version' => \Composer\InstalledVersions::getPrettyVersion(\Composer\InstalledVersions::getRootPackage()['name']),
         ]));
